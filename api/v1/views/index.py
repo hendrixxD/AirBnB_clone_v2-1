8cd2befa00dca303d1dfaec384eb57bc96827f91
hendrixxD <maticsjnr@gmail.com>
2022-12-16 15:46:38 +0100

    an endpoint that retrieves number of objec types

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,34 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
-@app.app_views("/status")
-def indx():
+@app_views.route("/status", strict_slashes=False)
+def status():
+    """
+    return the status of application
+    """
     return(jsonify{"status":"Ok"})
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """
+    Retrieves count of objects in storage
+    """
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+
+    classes = {"amenities": Amenity, "cities": City,
+            "Places": Place, "reviews": Review,
+            "states": State, "users": User}
+
+    _dict = {}
+
+    for name, cls in classes.items():
+        _dict.update({name: storage.count(cls)})
+
+    return jsonify(_dict)
